9d432432f4e4ae5354fc20a3cd01d633d9a2f5c6
Stanislav <115217864+Web-dev2022@users.noreply.github.com>
2023-02-27 11:47:25 +0200

    my resolve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
-class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+ class GuessingGame {
+  constructor() {}
+  
+  setRange(min, max) {
+    this.left = min;
+    this.right = max;
+  }
+  
+  guess() {
+    this.mid = Math.round((this.right - this.left) / 2) + this.left;
+    return this.mid;
+  }
+  
+  lower() {
+    this.right = this.mid - 1;
+  }
+  
+  greater() {
+    this.left = this.mid + 1;
+  }
 }
 
 module.exports = GuessingGame;
